d4a1c79ad902d6f15ac1601e754c1d9808ec2e14
2459005-dot <2459005@kbu.ac.kr>
2025-09-30 21:43:38 +0900

    .

--- frontend/src/utils/contact.js
+++ frontend/src/utils/contact.js
@@ -3,22 +3,22 @@ const contact = {
         {
             icon: 'call',
             label: '전화',
-            value: '010-1234-5678',
-            href: 'tel:01012345678',
+            value: '010-7140-4693',
+            href: 'tel:01071404693',
             hint: '평일 09:00–18:00 / 점심 12:00–13:00'
         },
         {
             icon: 'mail',
             label: '이메일',
-            value: 'hello@liz.dev',
-            href: 'mailto:hello@liz.dev',
+            value: '2459005@kbu.ac.kr',
+            href: 'mailto:2459005@kbu.ac.kr',
             hint: '24시간 접수 · 1영업일 내 회신'
         },
         {
             icon: 'location_on',
             label: '주소',
-            value: '서울특별시 강남구 삼성동 123',
-            href: 'https://naver.me/지도링크',
+            value: '경기도 남양주시 진접읍 경복대로 지하244',
+            href: 'https://map.naver.com/p/search/%EC%A7%84%EC%A0%91%EC%97%AD/subway-station/405?c=15.00,0,0,0,dh',
             hint: '방문 미팅은 예약 후 가능합니다.'
         },
     ],
@@ -26,14 +26,14 @@ const contact = {
         {
             icon: 'link',
             label: 'GitHub',
-            value: 'github.com/username',
-            href: 'https://github.com/username'
+            value: 'github.com/2459005-dot',
+            href: 'https://github.com/2459005-dot'
         },
         {
             icon: 'link',
             label: 'Notion',
             value: 'notion.site/portfolio',
-            href: 'https://notion.site/portfolio'
+            href: 'https://www.notion.so/8ec12a5ab25a412fb66cd5d2b40dd69f'
         },
     ]
 };
